50f88173e98b2b95a8440a83d16ca1963cf3cddc
Neyro Caraig <neyrocaraig@gmail.com>
2026-05-19 07:44:50 +0800

    feat: added card visual adaptors

--- scripts/resources/card_data.gd
+++ scripts/resources/card_data.gd
@@ -4,4 +4,5 @@ extends Resource
 @export var card_name: String = ""
 @export var type: Enums.CardTypeEnum = Enums.CardTypeEnum.TRICK
 @export var mana_cost: int = 0
+@export var description: String = ""
 @export var effects: Array[EffectData] = []
