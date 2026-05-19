fde3a4c6ae2f12d3c537bbfa1dee63becb51e676
Keith Ashly Domingo <thirdwheelerforhire@gmail.com>
2026-05-19 13:50:11 +0800

    feat: Added background mmusic and title screen design

--- CREDITS.md
+++ CREDITS.md
@@ -5,9 +5,6 @@
 - [Pixabay](https://pixabay.com/) - Free Sound effects
 
 ## More Assets
-- [chasersgaming](https://chasersgaming.itch.io/asset-pack-8-ball-pool-sms) - Placeholder Marble Sprites
-- [Luminous Dice](https://luminous-dice.itch.io/free-pixel-art-card-template-pack) - Placeholder Card Sprites
-- [Shiroze](https://shiroze.itch.io/office-background) - Placeholder Background
 - [Otto Ajala](https://ottoojala.itch.io/pirkkala) - Font
 - [dani maccari](https://dani-maccari.itch.io/jumppack-music-loops) - Background Jams
     
--- scripts/ui/main_menu.gd
+++ scripts/ui/main_menu.gd
@@ -2,14 +2,13 @@ extends Control
 
 @onready var _online_button: Button = %OnlineButton
 @onready var _offline_button: Button = %OfflineButton
-@onready var _deck_builder_button: Button = %DeckBuilderButton
 @onready var _quit_button: Button = %QuitButton
 
 func _ready() -> void:
 	_online_button.pressed.connect(_on_online_pressed)
 	_offline_button.pressed.connect(_on_offline_pressed)
-	_deck_builder_button.pressed.connect(_on_deck_builder_pressed)
 	_quit_button.pressed.connect(_on_quit_pressed)
+	$BackgroundMusic.play()
 
 func _on_online_pressed() -> void:
 	get_tree().change_scene_to_file("res://scenes/ui/matchmaking_lobby.tscn")
